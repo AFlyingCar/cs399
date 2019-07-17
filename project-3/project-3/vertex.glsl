6cb2b3b8b23de637c43e3960b9bd27bbccd2d1a8
Tyler Robbins <tyler@familyrobbins.com>
2019-07-17 16:02:21 -0700

    Added transformation uniform.

--- project-3/project-3/vertex.glsl
+++ project-3/project-3/vertex.glsl
@@ -3,11 +3,12 @@
 layout(location=0) in vec4 in_Position;
 layout(location=1) in vec4 in_Color;
 
+uniform mat4 transform;
+
 out vec4 ex_Color;
 
 void main(void)
 {
-  gl_Position = in_Position;
-  //gl_Position = in_Position;
-  ex_Color = in_Color;
+  gl_Position = transform * in_Position;
+  ex_Color = in_Color; // vec4((transform * in_Position).rgb, 1.0);
 }
